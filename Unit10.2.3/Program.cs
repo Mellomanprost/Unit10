b9c553ebe145ddd31f1e2ce96f2fdf1c793a1d17
Mellomanprost <anoppa@mail.ru>
2022-10-21 22:58:49 +0300

    Add exercise 10.2.2-10.2.4

---
+++ Unit10.2.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit10._2._3
+{
+    public interface IWorker
+    {
+        void Build();
+    }
+    public class Worker: IWorker
+    {
+        public void Build()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.Build();
+        }
+    }
+}
---
+++ Unit10.2.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit10._2._4
+{
+    public interface IWorker
+    {
+        void Build();
+    }
+    public class Worker: IWorker
+    {
+        void IWorker.Build()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            ((IWorker)worker).Build();
+        }
+    }
+}
