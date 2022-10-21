ce73c88e1ef91800bfebad0378d208f663dfbc99
Mellomanprost <anoppa@mail.ru>
2022-10-21 22:50:52 +0300

    Добавьте файлы проекта.

---
+++ Unit10.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit10._2
+{
+    public interface IWriter
+    {
+        void Write();
+    }
+    public class Writer : IWriter 
+    {
+        void IWriter.Write()
+        {
+            Writer writer = new Writer();
+            ((IWriter)writer).Write();
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
