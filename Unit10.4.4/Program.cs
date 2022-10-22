669f7f118b5828e11b25a8d2e65817a754a925d4
Mellomanprost <anoppa@mail.ru>
2022-10-22 23:25:15 +0300

    Added exercise 10.4, 10.4.4

---
+++ Unit10.4.4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit10._4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            var account = new Account();
+
+            IUpdater<Account> updater = new UserService();
+
+            var userService = new UserService();
+            userService.Update(user);
+        }
+        public interface IUpdater<in T>
+        {
+            void Update(T entity);
+        }
+
+        public class UserService : IUpdater<User>
+        {
+            public void Update(User entity)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class User
+        {
+
+        }
+
+        public class Account : User
+        {
+
+        }
+    }
+}
---
+++ Unit10.4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit10._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessenger<Phone> viberInPhone = new Viber<Phone>();
+            IMessenger<IPhone> viberInIPhone = new Viber<IPhone>();
+            IMessenger<Phone> viberInIphone = new Viber<IPhone>();
+
+            viberInPhone.DeviceInfo();
+            viberInIphone.DeviceInfo();
+            Console.Read();
+        }
+
+        public interface IMessenger<out T>
+        {
+            T DeviceInfo();
+        }
+
+        public class Phone { }
+
+        public class IPhone : Phone { }
+
+        public class Viber<T> : IMessenger<T> where T : Phone,
+        new()
+        {
+            public T DeviceInfo()
+            {
+                T device = new T();
+                Console.WriteLine(device);
+                return new T();
+            }
+        }
+    }
+}
